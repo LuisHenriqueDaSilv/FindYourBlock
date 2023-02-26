be1aff0b63e623d5670aecdeb2eebe4f818053df
LuisHenriqueDaSilv <luishenriquen14@gmail.com>
2023-02-26 10:56:11 -0300

    create

---
+++ web/components/AppHeader/index.tsx
@@ -0,0 +1,106 @@
+import { useState } from 'react'
+
+import styles from './styles.module.scss'
+
+import states from '../../datas/states.json'
+import { useRouter } from 'next/router'
+
+export function AppHeader() {
+
+    const {pathname} = useRouter()
+
+    const [blockState, setBlockState] = useState<string>('')
+    const [name, setName] = useState<string>('')
+    const [position, setPosition] = useState({lat: -15.7993786, lng: -47.8654648})
+
+    function handleSelectState(event: React.ChangeEvent<HTMLSelectElement>){
+
+        if(!event.target.value){
+            setBlockState('')
+            setPosition({lat: -15.7993786, lng: -47.8654648})
+            return 
+        }
+        const [lat, lng, state, uf] = event.target.value.split('/')
+
+        setBlockState(`${state}-${uf}`)
+        setPosition({lat:Number(lat), lng:Number(lng)})
+    }
+
+
+    return (
+        <div id={styles.container}>
+            <img
+                src="/bottomHeader.png"
+                alt="Imagem carnavalesca"
+                id={styles.bottomImage}
+                className={styles.backgroundImage}
+            />
+            <img
+                src="/topHeader.png"
+                alt="Imagem carnavalesca"
+                id={styles.topImage}
+                className={styles.backgroundImage}
+            />
+
+            <div id={styles.wrapper}>
+
+
+                <p id={styles.appName}>
+                    FIND YOUR BLOCK
+                </p>
+
+                <h1 id={styles.title}>
+                    Encontre os <span>melhores blocos </span>
+                    de carnaval de 2023
+                </h1>
+
+                <form
+                    id={styles.searchContainer}
+                    onSubmit={(event) => event.preventDefault()}
+                >
+
+                    <div className={styles.input}>
+                        <img
+                            src="/search.png"
+                            alt="Icone de busca"
+                        />
+                        <input
+                            onChange={(event) => setName(event.target.value)}
+                            type="text"
+                            placeholder="Pesquise por nome"
+                        />
+                    </div>
+
+                    <div className={styles.input} id={styles.locationSelect}>
+                        <img
+                            src="/location.png"
+                            alt="Icone de localização"
+                        />
+                        <select
+                            onChange={handleSelectState}
+                        >
+                            <option value="">Selecione uma cidade</option>
+                            {
+                                states.map((state) => {
+                                    return (
+                                        <option
+                                            value={`${state.latitude}/${state.longitude}/${state.name}/${state.uf}`}
+                                            key={state.name}
+                                        >
+                                            {`${state.name}-${state.uf}`}
+                                        </option>
+                                    )
+                                })
+                            }
+                        </select>
+                    </div>
+
+                    <a
+                        href={`${`${pathname}?estado=${blockState}&nome=${name}&lat=${position.lat}&lng=${position.lng}`}`}
+                        id={styles.submitButton}
+                    >BUSCAR AGORA</a>
+                </form>
+            </div>
+        </div>
+    )
+}
--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import {AppHeader} from '../components/AppHeader'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <AppHeader/>
+      <Component {...pageProps} />
+    </>
+  )
 }
+
+export default MyApp
